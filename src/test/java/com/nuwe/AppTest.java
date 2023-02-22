0e504768fcede18b6c6768db338a031dceb5a850
Andreu Nuwe <andreu@nuwe.io>
2023-02-22 10:03:41 +0100

    More coverage

--- src/test/java/com/nuwe/AppTest.java
+++ src/test/java/com/nuwe/AppTest.java
@@ -7,14 +7,26 @@ public class AppTest {
 
   @Test
   public void firstTest() {
-    String[] strs = {"flower","flow","flight"};
+    String[] strs = {"flower", "flow", "flight"};
     assertEquals("fl", App.longestCommonPrefix(strs));
   }
 
   @Test
-  public void secondTest() {
-    String[] strs = {"dog","racecar","car"};
+  public void voidTest() {
+    String[] strs = {"dog", "racecar", "car"};
     assertEquals("", App.longestCommonPrefix(strs));
+  }
+
+  @Test
+  public void endTest() {
+    String[] strs = {"dog", "donkey", "do"};
+    assertEquals("do", App.longestCommonPrefix(strs));
+  }
+
+  @Test
+  public void fullEqual() {
+    String[] strs = {"dog", "dog", "dog"};
+    assertEquals("dog", App.longestCommonPrefix(strs));
   }
 
 }
